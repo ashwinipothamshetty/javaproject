ae9acac178d06ec849514f6fbd7c2d4025c82e09
ashwinipothamshetty <ashwini1841999@gmail.com>
2024-07-24 20:21:47 +0530

    Add files via upload

---
+++ ShristiAcademy/src/com/oops/overload/Shape.java
@@ -0,0 +1,21 @@
+package com.oops.overload;
+
+public class Shape {
+
+	void area(int x) {
+		System.out.println("square " + x * x);
+	}
+
+	/*
+	 * void area(float x) { System.out.println("circle " + (Math.PI * x * x)); }
+	 */
+
+	int area(int x, int y) {
+		return x * y;
+	}
+	
+	double area(int x,float y) {
+		return x*y*0.5;
+	}
+
+}
---
+++ ShristiAcademy/src/com/oops/overload/VarargDemo.java
@@ -0,0 +1,29 @@
+package com.oops.overload;
+
+public class VarargDemo {
+
+	
+	
+	void calsum(String name,int... nums) {
+		int sum=0;
+		for(int num:nums) {
+			sum+=num;
+			
+		}
+		System.out.println(sum);
+	
+
+}
+
+	void calsum(String... name) {
+		
+		System.out.println(name);
+	}
+	
+	public static void main(String[] args) {
+		
+		VarargDemo demo= new VarargDemo();
+		demo.calsum("ashwini",5,6,8);
+		demo.calsum("sonu","ash");
+}
+}
