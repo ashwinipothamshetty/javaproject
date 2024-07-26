3ddd4c31e29394cf66a2ce2e96c767e0d8519710
ashwinipothamshetty <ashwini1841999@gmail.com>
2024-07-26 15:12:24 +0530

    Update MainCalculator.java

--- Assignments/src/com/shristi/intface2/MainCalculator.java
+++ Assignments/src/com/shristi/intface2/MainCalculator.java
@@ -3,8 +3,24 @@ package com.shristi.intface2;
 public class MainCalculator {
 
 	public static void main(String[] args) {
-		Calculator calci = (x,y) -> System.out.println(x + y);
+		Calculator calci = (x, y) -> System.out.println(x + y);
 		calci.calculate(10, 100);
+
+		calci = (x, y) -> {
+			System.out.println(x - y);
+		};
+		calci.calculate(100, 10);
+
+		calci = (x, y) -> {
+			System.out.println(x * y);
+		};
+		calci.calculate(10, 5);
+
+		calci = (x, y) -> {
+			System.out.println(x / y);
+
+		};
+		calci.calculate(10, 2);
 	}
 
 }
